d937d057cbfb4727d40916e221ad0dffda00e8b2
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-03-17 23:26:10 -0500

    update

--- contracts/tokens.sol
+++ contracts/tokens.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Link is ERC20 {
     constructor() ERC20("Chainlink", "LINK") public {
-        _mint(msg.sender, 2000);
+        _mint(msg.sender, 1e24); 
     }
 }
